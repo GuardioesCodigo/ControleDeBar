48cefbd105f7bea150492214f260804527a43f2c
buenoIago <iagocedup@gmail.com>
2026-08-18 16:25:17 -0300

    feat: implementa apresentação do Garçom

---
+++ src/ControleDeBar.WebApp/Modulos/ModuloGarcom/GarcomProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using ControleDeBar.Aplicacao.Modulos.ModuloGarcom;
+
+namespace ControleDeBar.WebApp.Modulos.ModuloGarcom;
+
+public class GarcomProfile : Profile
+{
+    public GarcomProfile()
+    {
+        CreateMap<ListarGarcomDto, ListarGarcomViewModel>();
+        CreateMap<CadastrarGarcomViewModel, CadastrarGarcomDto>();
+        CreateMap<DetalhesGarcomDto, EditarGarcomViewModel>();
+        CreateMap<DetalhesGarcomDto, ExcluirGarcomViewModel>();
+    }
+}
