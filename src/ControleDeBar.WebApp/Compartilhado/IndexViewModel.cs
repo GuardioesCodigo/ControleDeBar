076f6d17033b843579863f8a0073144cba0c3a60
buenoIago <iagocedup@gmail.com>
2026-08-21 21:09:08 -0300

    versão final do projeto

--- src/ControleDeBar.WebApp/Compartilhado/IndexViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ControleDeBar.WebApp.Compartilhado;
-
-public record IndexViewModel(
-    string NomeEstabelecimento,
-    int ContasAbertas,
-    decimal FaturamentoDoDia,
-    int MesasOcupadas,
-    int TotalMesas
-);
